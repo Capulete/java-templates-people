63f965544404eabda7b700169a62a5c079f95c7e
Помойка <capulete@mail.ru>
2023-01-19 17:08:47 +0300

    Initial commit

---
+++ src/Person.java
@@ -0,0 +1,71 @@
+import java.util.Objects;
+import java.util.OptionalInt;
+
+public class Person {
+    protected final String name;
+    protected final String surname;
+    protected int age;
+    protected String adress;
+
+    public Person(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public Person(String name, String surname, int age) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+    }
+
+    public PersonBuilder newChildBuilder() {
+        return new PersonBuilder().setSurname(surname).setAdress(adress);
+    }
+
+    public boolean hasAge() {
+        return getAge().isPresent() && getAge().getAsInt() > 0;
+    }
+
+    public boolean hasAddress() {
+        return !getAdress().isEmpty();
+    }
+
+    public void happyBirthday() {
+        if (hasAge()) age++;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public OptionalInt getAge() {
+        return OptionalInt.of(age);
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", age=" + age +
+                ", adress='" + adress + '\'' +
+                '}';
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, surname, age, adress);
+    }
+}
